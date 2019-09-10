2aeecf71935884fbaf6b870d1f871a5e558043c0
Nick <naaltunian@icloud.com>
2019-09-09 20:35:13 -0400

    Set up express server and dependencies

---
+++ intermediate/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const { ApolloServer, gql } = require('apollo-server');
+// const typeDefs = require('./typeDefs');
+// const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
