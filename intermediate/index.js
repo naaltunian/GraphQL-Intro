f11932e1de8f304dcad9b9b8747cedb6c6222488
Nicholas Altunian <naaltunian@icloud.com>
2019-09-16 21:20:59 -0400

    Log error if database doesn't connect

--- intermediate/index.js
+++ intermediate/index.js
@@ -15,7 +15,7 @@ const User = require('./Models/User');
 // database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .then(() => console.log('DB connected'))
-    .catch(err => console.error());
+    .catch(err => console.error(err));
 
 // graphQL server
 const server = new ApolloServer({
